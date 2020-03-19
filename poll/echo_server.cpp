ebd4bc7dd60c9372670f13a4f5999822b84643b5
fidiler <568200539@qq.com>
2020-03-19 19:49:04 +0800

    commit

---
+++ poll/echo_server.cpp
@@ -0,0 +1,141 @@
+#include <iostream>
+#include <vector>
+
+#include <cstring>
+
+#include <poll.h>
+#include <unistd.h>
+#include <errno.h>
+#include <arpa/inet.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+
+using std::cout;
+using std::endl;
+using std::string;
+
+int createServer(in_addr_t addr, in_port_t port, int backlog) {
+    int fd = socket(AF_INET, SOCK_STREAM, 0);
+    if (fd == -1) {
+        return fd;
+    }
+
+    sockaddr_in serverAddr;
+    memset(&serverAddr, 0, sizeof(sockaddr_in));
+    serverAddr.sin_family = AF_INET;
+    serverAddr.sin_addr.s_addr = addr;
+    serverAddr.sin_port = port;
+
+    int callRet;
+    callRet = bind(fd, (const sockaddr*)&serverAddr, sizeof(sockaddr_in));
+    if (callRet == -1) {
+        return callRet;
+    }
+
+    callRet = listen(fd, backlog);
+    if (callRet == -1) {
+        return callRet;
+    }
+}
+
+void addFd(pollfd *ev, int fd, int pos, short evs) {
+    ev[pos].fd = fd;
+    ev[pos].events = evs;
+    ev[pos].revents = 0;
+}
+
+bool checkAccept(pollfd *ev, int evSize, int& pos) {
+    if (ev[0].revents & POLLRDNORM) {
+        sockaddr_in clientAddr;
+        socklen_t clientAddrLen;
+        int clientFd = accept(ev[0].fd, (sockaddr*)&clientAddr, &clientAddrLen);
+        if (clientFd == -1) {
+            cout << "accept error: " << strerror(errno) << endl;
+            return false;
+        }
+
+        int i;
+        for (i = 1; i < evSize; i++) {
+            if (ev[i].fd < 0) {
+                ev[i].fd = clientFd;
+                ev[i].events = POLLRDNORM;
+            }
+        }
+
+        if (i == evSize) {
+            cout << "too many connect" << endl;
+            return false;
+        }
+
+        if (i > pos) {
+            pos = i;
+        }
+    }
+
+    return true;
+}
+
+void echo(pollfd *ev, int i) {
+    if (ev[i].revents & POLLRDNORM) {
+
+    }
+}
+
+int runServer(int serverFd) {
+    nfds_t evSize = 1024;
+    pollfd ev[evSize];
+    int evPos = 0;
+    int pollReady;
+
+    addFd(ev, serverFd, evPos, POLLRDNORM);
+    for (int i = 0; i < evSize; i++) {
+        ev[i].fd = -1;
+    }
+
+    while(true) {
+        pollReady = poll(ev, evSize, -1);
+        if (pollReady == -1) {
+            cout << "poll error: " << strerror(errno) << endl;
+            break;
+        }
+
+        cout << "poll ready: " << pollReady << endl;
+        if (!checkAccept(ev, evSize, evPos)) {
+            continue;
+        }
+
+        if (--pollReady <= 0) {
+            continue;
+        }
+
+
+
+    }
+}
+
+int main(int argc, const char *argv[]) {
+    in_addr_t addr;
+    in_port_t port;
+    string listenInfo;
+
+    if (argc != 3) {
+        addr = INADDR_ANY;
+        port = htons(34567);
+        listenInfo = "0.0.0.0:34567";
+    } else {
+        inet_pton(AF_INET, argv[1], &addr);
+        port = htons(atoi(argv[2]));
+        listenInfo = argv[1];
+        listenInfo.append(":");
+        listenInfo.append(argv[2]);
+    }
+
+    int serverFd = createServer(addr, port, 128);
+    if (serverFd == -1) {
+        cout << "listen server error: " << strerror(errno) << endl;
+        return 0;
+    }
+
+    cout << listenInfo << endl;
+    runServer(serverFd);
+}
--- select_tcp_echo_server.cpp
+++ select_tcp_echo_server.cpp
@@ -9,8 +9,113 @@
 
 #include <vector>
 #include <iostream>
+#include <deque>
 
 using std::vector;
+using std::deque;
+
+
+
+class client {
+public:
+    client() = default;
+
+    client(int connfd) : __conn(connfd) {}
+
+    inline int get_conn() const { return __conn; };
+
+    inline void set_conn(int connfd) { __conn = connfd; };
+
+private:
+    int __conn;
+};
+
+void serverLoop(int serverfd) {
+    fd_set rset, allset;
+    int clientUsage = 0;
+    int readn, writen;
+    int selectReady, maxfd = serverfd;
+
+    FD_ZERO(&allset);
+    FD_SET(serverfd, &allset);
+
+    const int BUF_SIZE = 1024;
+    char buf[BUF_SIZE];
+
+    vector<int> clients(FD_SETSIZE, -1);
+
+    for (;;) {
+        rset = allset;
+        selectReady = select(maxfd + 1, &rset, nullptr, nullptr, nullptr);
+        if (selectReady == -1) {
+            std::cout << "select failure." << std::endl;
+            break;
+        }
+
+        std::cout << "select ready: " << selectReady << std::endl;
+
+        // 在服务器fd上发生了可读事件
+        if (FD_ISSET(serverfd, &rset)) {
+            std::cout << "ready fd: " << serverfd << std::endl;
+            int connFd;
+            struct sockaddr_in connAddr;
+            socklen_t connAddrLen = sizeof(struct sockaddr_in);
+            connFd = accept(serverfd, (struct sockaddr *) &connAddr, &connAddrLen);
+            std::cout << "conn fd: " << connFd << std::endl;
+
+            for (int i = 0; i < clients.size(); i++) {
+                if (clients[i] == -1) {
+                    clients[i] = connFd;
+                    clientUsage = i;
+                    break;
+                }
+            }
+
+            // 添加监听的fd
+            FD_SET(connFd, &allset);
+
+            // 当前能够监听的客户端数量超过select的最大值
+            if (clientUsage == FD_SETSIZE) {
+                std::cout << "too many connect." << std::endl;
+            }
+
+            //细节：有新的客户端加入连接后，设置当前select监听的最大的fd
+            if (connFd > maxfd) {
+                maxfd = connFd;
+            }
+
+            //细节：server监听到了有客户端连接，accept掉后，该事件就算被处理了，但此时新建立
+            // 连接的客户端可能还没有发送数据，因此需要continue
+            // 也有可能同时发生了已经建立好连接的客户端send数据，同时有另一个陌生的客户端建立连接，此时select ready可能是2,
+            // 因此accept掉客户端连接事件后，还需要继续处理之前已经连接好的客户端send的数据
+            if (--selectReady <= 0) {
+                continue;
+            }
+        }
+
+        // 在其他描述符中发生了可读事件
+        for (int i = 0; i < clients.size(); i++) {
+            if (clients[i] == -1) {
+                continue;
+            }
+
+            // 测试是否是该客户端的fd发生了可读事件
+            if (FD_ISSET(clients[i], &rset)) {
+                readn = recv(clients[i], buf, BUF_SIZE, 0);
+                if (readn == 0) {
+                    std::cout << "client close connect." << std::endl;
+                    FD_CLR(clients[i], &allset);
+                    close(clients[i]);
+                    clients[i] = -1;
+
+                } else if (readn > 0) {
+                    send(clients[i], buf, readn, 0);
+                    memset(buf, '\0', readn);
+                }
+            }
+        }
+    }
+}
 
 int main(int argc, const char *argv[]) {
     int servfd = socket(AF_INET, SOCK_STREAM, 0);
@@ -34,66 +139,5 @@ int main(int argc, const char *argv[]) {
         return 0;
     }
 
-    fd_set rset;
-    FD_ZERO(&rset);
-    FD_SET(servfd, &rset);
-    int maxFd = servfd;
-    vector<int> clients(FD_SETSIZE, -1);
-    vector<int>::size_type maxClientSize = 0;
-    int readyFD;
-
-
-
-    char buf[1024];
-    memset(buf, '\0', 1024);
-
-    int readn;
-    for (;;) {
-        readyFD = select(maxFd + 1, &rset, nullptr, nullptr, nullptr);
-        if (readyFD == -1) {
-            std::cout << "select failure." << std::endl;
-            break;
-        }
-
-        std::cout << readyFD << std::endl;
-
-        if (FD_ISSET(servfd, &rset)) {
-            int connFd;
-            struct sockaddr_in connAddr;
-            socklen_t connAddrLen = sizeof(struct sockaddr_in);
-            connFd = accept(servfd, (struct sockaddr *) &connAddr, &connAddrLen);
-            // 设置连接的客户端的fd
-            for (vector<int>::size_type i = 0, n = clients.size(); i < n; i++) {
-                if (clients[i] == -1) {
-                    clients.at(i) = connFd;
-                    maxClientSize = i;
-                    break;
-                }
-            }
-            FD_SET(connFd, &rset);
-
-            if (maxClientSize == clients.size()) {
-                std::cout << "too many connection." << std::endl;
-                continue;
-            }
-        }
-
-        for (vector<int>::size_type i = 0, n = clients.size(); i < n; i++) {
-            if (clients[i] < 0) {
-                continue;
-            }
-            if (FD_ISSET(clients[i], &rset)) {
-                // 客户端关闭连接
-                readn = recv(clients[i], buf, 1024, 0);
-                if (readn == 0) {
-                    clients.at(i) = -1;
-                    FD_CLR(clients[i], &rset);
-
-                } else if (readn > 0) {
-                    send(clients[i], buf, readn, 0);
-                    memset(buf, '\0', readn);
-                }
-            }
-        }
-    }
+    serverLoop(servfd);
 }
